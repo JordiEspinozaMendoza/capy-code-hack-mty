48f8d4f332a8cdddf40e1dc0b0eed43b1d1ffbe2
HectorLN <hector.lucero193@tectijuana.edu.mx>
2023-09-23 14:52:34 -0600

    Initialized Next and created Code Editor

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,22 @@ import { javascript } from '@codemirror/lang-javascript';
 import { vscodeDark, vscodeDarkInit } from '@uiw/codemirror-theme-vscode';
 
 function Editor() {
+  //Code Editor
   const [value, setValue] = React.useState("console.log('hello world!');");
   const onChange = React.useCallback((val, viewUpdate) => {
     console.log('val:', val);
     setValue(val);
   }, []);
-  return <CodeMirror value={value} height="800px" width="100" extensions={[javascript({ jsx: true })]} onChange={onChange} theme={vscodeDark} />;
+  return (
+    <>
+      <div>
+      <CodeMirror value={value} height="800px" width="100" extensions={[javascript({ jsx: true })]} onChange={onChange} theme={vscodeDark} />
+    </div>
+
+    <div>
+      
+    </div>
+    </>
+  )
 }
 export default Editor;
