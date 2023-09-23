e247b74cfc3486a6f397f05f604c2608d0f9fe3f
leonardo mercado <leonardoomercado2@gmail.com>
2023-09-23 15:10:13 -0600

    prompt

--- index.js
+++ index.js
@@ -30,24 +30,34 @@ io.on("connection", async (socket) => {
 
 server.listen(3000, async () => {
   const codePrompt = `
-This is a technical interview and you are the interviewer. Please find any issues/errors (sintaxis/logic) in the following code, if you cant find any give a suggestion to the user on how to improve the code (note: do not send me any code snippet, just natural language).
+This is a technical interview and you are the interviewer. Please find any issues/errors (syntax [this goes in "syntaxIssues"]/logic[this goes in "logicIssues"]) in the following code, if you cant find any give a suggestion to the user on how to optimize the code [this goes in "suggest"] (note: do not send me any code snippet, just natural language).
+If you cant find any issues or improvement send back a positive feedback message [this goes in feedback].
 
+Code starts here
 io.on("connection", async (socket) => {
     console.log("a user connected");
+
+    
     const newDate = new Date();
 
     if(newDate){
         console.log(newDate.toDateString());
     }
+  
+
   });
+
+  Code ends here.
 
   Give your suggestion in this format:
   {
     "suggest": yourSuggestions,
     "logicIssues": yourLogicIssues,
     "syntaxIssues": yourSyntaxIssues
+    "feedback": yourFeedback
   }
 }
+
 `;
 
   console.log(await createSuggestion(codePrompt));
