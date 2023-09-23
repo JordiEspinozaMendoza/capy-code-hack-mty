7e7960d83a9d26c9be31852c248b2e3829c2068b
leonardo mercado <leonardoomercado2@gmail.com>
2023-09-23 17:04:09 -0600

    redis

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const { createSuggestion } = require("./utils");
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+// TODO: Replace the following with your app's Firebase project configuration
+const { createClient } = require("redis");
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+client.on("connect", () => console.log("Connected to Redis!"));
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+
+
+
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
@@ -29,6 +40,7 @@ io.on("connection", async (socket) => {
 });
 
 server.listen(3000, async () => {
+  let logic = "";
   const codePrompt = `
 This is a technical interview and you are the interviewer. Please find any issues/errors (syntax [this goes in "syntaxIssues"]/logic[this goes in "logicIssues"]) in the following code, if you cant find any give a suggestion to the user on how to optimize the code [this goes in "suggest"] (note: do not send me any code snippet, just natural language).
 If you cant find any issues or improvement send back a positive feedback message [this goes in feedback].
@@ -60,7 +72,36 @@ io.on("connection", async (socket) => {
 
 `;
 
-  console.log(await createSuggestion(codePrompt));
+const response = await createSuggestion(codePrompt);
+let suggestion = [];
+suggestion.push(JSON.parse(response[0].message.content)["suggest"] ); 
+
+// logic
+
+await client.connect();
+const idTemp = "1"
+await client.hSet(idTemp, {
+  logic: [JSON.parse(response[0].message.content)["logicIssues"]],
+  logicCount: 1,
+  
+})
+// await client.hSet(idTemp, {
+//   logic: 'John',
+//   logicCount: 1,
+
+//   syntax: 'Smith',
+//   syntaxCount: 1,
+
+//   suggestion: 'Redis',
+//   suggestionCount: 1,
+
+//   feedback: '29',
+  
+// })
+
+// let userSession = await client.hGetAll(idTemp);
+// console.log(JSON.stringify(userSession, null, 2)); 
+
 
   console.log("server running at http://localhost:3000");
 });
