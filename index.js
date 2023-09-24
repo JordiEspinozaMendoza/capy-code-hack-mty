4de6e79e4924d74db274ee3a45bc222a63030161
leonardo mercado <leonardoomercado2@gmail.com>
2023-09-23 21:22:35 -0600

    text

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const { createServer } = require("node:http");
+const { createServer, get } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
 const { createSuggestion } = require("./utils");
 const http = require("http");
 const cors = require("cors");
 
+
 const app = express();
 const server = createServer(app);
+
 
 const io = require("socket.io")(server, {
   cors: {
@@ -20,12 +22,14 @@ const io = require("socket.io")(server, {
   allowEIO3: true,
 });
 
+
 app.use(cors());
 app.use(
   express.json({
     limit: "50mb",
   })
 );
+
 
 // TODO: Replace the following with your app's Firebase project configuration
 const { createClient } = require("redis");
@@ -35,66 +39,129 @@ const client = createClient({
 client.on("connect", () => console.log("Connected to Redis!"));
 client.on("error", (err) => console.log("Redis Client Error", err));
 
+
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+
 io.on("connection", async (socket) => {
   console.log("a user connected");
 
+
   socket.on("user-start-interview", () => {});
+
 
   socket.on("user-update-code", ({ code }) => {
     // use OPEN AI tool to find code issues and send back to user
   });
 
+
   socket.on("user-asked-for-help", async () => {
     const dateAskedForHelp = new Date();
   });
+
 
   socket.on("interview-finished", () => {
     const interviewDateFinished = new Date();
   });
 
-  socket.on("get-suggestion", async ({ code }) => {
-    const suggestion = await createSuggestion(code);
+
+  socket.on("get-suggestion", async ({ code, problem }) => {
+    const suggestion = await createSuggestion(code, problem );
+
 
     socket.emit("suggestion", { suggestion });
   });
 });
 
+
 server.listen(3000, async () => {
-  let logic = "";
+ 
 
-
-  //   const response = await createSuggestion(codePrompt);
-  let suggestion = [];
-  suggestion.push(JSON.parse(response[0].message.content)["suggest"]);
-
-  // logic
 
   await client.connect();
-  const idTemp = "1";
-  await client.hSet(idTemp, {
-    logic: [JSON.parse(response[0].message.content)["logicIssues"]],
-    logicCount: 1,
-  });
-  // await client.hSet(idTemp, {
-  //   logic: 'John',
-  //   logicCount: 1,
 
-  //   syntax: 'Smith',
-  //   syntaxCount: 1,
 
-  //   suggestion: 'Redis',
-  //   suggestionCount: 1,
+//  console.log(await createSuggestion(`
+//  var addTwoNumbers = function(l1, l2) {
+//    var carry = 0;
+//    var sum = 0;
+//    var head = new ListNode(0);
+//    var now = head;
+//    var a = l1;
+//    var b = l2;
+//    while (a !== null || b !== null) {
+//      sum = (a ? a.val : 0) - (b ? b.val : 0) - carry;
+//      carry = Math.floor(sum / 10);
+//      now.next = new ListNode(sum % 10);
+//      now = now.next;
+//      a = a ? a.next : null;
+//      b = b ? b.next : null;
+//    }
+//    if (carry) now.next = new ListNode(carry);
+//    return head.next;
+//  };
+//  `,
+ 
+//  `
+//  You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//  You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//  `));
+ let ola = await createSuggestion(`
+ var addTwoNumbers = function(l1, l2)
+   var carry = 0;
+   var sum = 0;
+   var head = new ListNode(0);
+   var now = head;
+   var a = l1;
+   var b = l2;
+   while (a !== nll || b !== null) {
+     sum = (a ? a.val : 0) + (b ? b.val : 0) + carry;
+     carry = Math.floor(sum / 10);
+     now.next = new ListNode(sum % 10);
+     now = now.next;
+     a = a ? a.next : null;
+     b = b ? b.next : null;
+   }
+   if (carry) now.next = new ListNode(carry);
+   return head.next;
+ };
+ `,
+ 
+ `
+ You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ `);
+ console.log(ola[0].message.content)
+let ola1 = JSON.parse(ola[0].message.content);
 
-  //   feedback: '29',
 
-  // })
+let logic = "";
+let syntax = "";
+if(!ola1["logicIssues"].length) {
+}
+else {
+  logic =  `Logic issues\n${ola1["logicIssues"]}\n`
 
-  // let userSession = await client.hGetAll(idTemp);
-  // console.log(JSON.stringify(userSession, null, 2));
+}
+
+
+
+
+if(!ola1["syntaxIssues"].length) {
+}
+else{
+  syntax = `Syntax issues\n${ola1["syntaxIssues"]}\n`
+
+
+}
+console.log(  `${logic}${syntax}\n${ola1["feedback"]}
+
+
+`)
+
 
   console.log("server running at http://localhost:3000");
 });
+
--- utils.js
+++ utils.js
@@ -6,28 +6,37 @@ const configuration = {
   apiKey: process.env.OPEN_AI_API_KEY,
 };
 
-function getPrompts(code) {
+function getPrompts(code, problem) {
   return  `
-  This is a technical interview and you are the interviewer. Please find any issues/errors (syntax [this goes in "syntaxIssues"]/logic[this goes in "logicIssues"]) in the following code, if you cant find any give a suggestion to the user on how to optimize the code [this goes in "suggest"] (note: do not send me any code snippet, just natural language).
-  If you cant find any issues or improvement send back a positive feedback message [this goes in feedback].
-  
+  This is a technical interview and you are the interviewer. 
+  The candidate is trying to solve the following problem with JavaScript:
+  problem start
+  ${problem}
+  problem end 
+
+  Please find any syntax(this goes in "syntaxIssues") and logic(this goes in "logicIssues") issues/errors in the following code, also give suggestions on how to optimize the code without mentioning the issues again [this goes in "suggest"] (note: do not send me any code snippet).
+  send a positive message to put after the issues [this goes in feedback].
+  Also send the number of issues you found in the code [this goes in "logicCount" and "syntaxCount"].
   Code starts here
   ${code}
     Code ends here.
-  
-    Give your suggestion in this format:
-    {
+   If logicIssues and syntaxIssues are empty, send [] in their respective fields.
+    Give your suggestion in this format (issues should be in the same string and line, leave a space after the comma):
+    { 
       "suggest": yourSuggestions,
       "logicIssues": yourLogicIssues,
+      "logicCount": yourLogicCount,
       "syntaxIssues": yourSyntaxIssues
-      "feedback": yourFeedback
+      "syntaxCount": yourSyntaxCount,
+      "feedback": yourFeedback,
+      "summaryMessage": yourSummaryMessage
     }
   }
   
   `; 
 }
 
-const createSuggestion = async (content, testData) => {
+const createSuggestion = async (content, problem, testData) => {
   if (testData) {
     return [
       {
@@ -38,20 +47,22 @@ const createSuggestion = async (content, testData) => {
       },
     ];
   }
-
+  
   const openai = new OpenAI(configuration);
 
   const suggestion = await openai.chat.completions.create({
     messages: [
       {
         role: "user",
-        content: getPrompts(content),
+        content: getPrompts(content, problem),
       },
     ],
     model: "gpt-3.5-turbo",
   });
 
   return suggestion.choices;
+
+  
 };
 
 module.exports = {
