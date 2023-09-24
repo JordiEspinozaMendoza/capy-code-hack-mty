1089b08e6e5a2657ae6b18e24c36d4eeb06792ab
HectorLN <hector.lucero193@tectijuana.edu.mx>
2023-09-23 18:07:15 -0600

    Added Header

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,9 @@ import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { vscodeDark, vscodeDarkInit } from "@uiw/codemirror-theme-vscode";
 import "react-chat-elements/dist/main.css";
-import { MessageBox } from "react-chat-elements";
+import { MessageBox, Avatar } from "react-chat-elements";
+import IconButton from "@mui/material/IconButton";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 function Editor() {
   //Code Editor
@@ -15,27 +17,37 @@ function Editor() {
     setValue(val);
   }, []);
   return (
-    <div className="MainContent">
-      <div className="Editor">
-        <CodeMirror
-          value={value}
-          height="800px"
-          extensions={[javascript({ jsx: true })]}
-          onChange={onChange}
-          theme={vscodeDark}
-        />
-      </div>
+    <>
+      <header>
+        <img className="Logo" src="capybara.png" />
+        <p id="CompanyName">CapyCode</p>
+        <IconButton sx={{ color: "black" }}>
+          <HelpOutlineIcon />
+        </IconButton>
+      </header>
+      <div className="MainContent">
+        <div className="Editor">
+          <CodeMirror
+            value={value}
+            height="800px"
+            extensions={[javascript({ jsx: true })]}
+            onChange={onChange}
+            theme={vscodeDark}
+          />
+        </div>
 
-      <div className="Chat">
-        <MessageBox
-          position={"rights"}
-          type={"text"}
-          text={"Hello world"}
-          title={"John Doe"}
-          dateString={"Fri May 08 2020"}
-        />
+        <div className="Chat">
+          <MessageBox
+            position={"rights"}
+            type={"text"}
+            text={"Hello user"}
+            title={"CapyCode"}
+            dateString={"Fri May 08 2020"}
+            avatar="capybara.png"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 export default Editor;
