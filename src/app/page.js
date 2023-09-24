c377d29b5bd29023f6658afda3d671ed3ff49b03
Jordi <jordi8101@gmail.com>
2023-09-23 18:08:28 -0600

    fix: added some logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,59 @@
 "use client";
 import "./globals.css";
-import React from "react";
+import { useState, useCallback, useEffect } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { vscodeDark, vscodeDarkInit } from "@uiw/codemirror-theme-vscode";
 import "react-chat-elements/dist/main.css";
 import { MessageBox } from "react-chat-elements";
+import io from "socket.io-client";
+
+const socket = io.connect(process.env.NEXT_PUBLIC_SOCKET);
 
 function Editor() {
-  //Code Editor
-  const [value, setValue] = React.useState("console.log('hello world!');");
-  const onChange = React.useCallback((val, viewUpdate) => {
+  const [value, setValue] = useState("console.log('hello world!');");
+  const [interviewStarted, setInterviewStarted] = useState(false);
+
+  const onChange = useCallback((val, viewUpdate) => {
     console.log("val:", val);
+    socket.emit("user-update-code", {
+      code: val,
+    });
     setValue(val);
   }, []);
+
+  useEffect(() => {
+    const handleGetSuggestion = () => {
+      socket.emit("get-suggestion", {
+        code: value,
+        id_user: 2,
+      });
+    };
+
+    const interval = setInterval(() => {
+      if (interviewStarted) {
+        handleGetSuggestion();
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [value, interviewStarted]);
+
+  if (!interviewStarted) {
+    return (
+      <div className="InterviewLobby">
+        <h2>Welcome to your interview</h2>
+        <button
+          onClick={() => {
+            setInterviewStarted(true);
+            socket.emit("user-start-interview");
+          }}
+        >
+          Start Interview
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="MainContent">
       <div className="Editor">
